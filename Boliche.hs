fd766646e5089b16e6c0f54071ab14b0b6951928
cIrigoyen16 <cyrigoyen969@gmail.com>
2023-05-21 12:40:23 -0300

    caso de prueba del objetivo 3  completado(marcos ya se siente piola)

--- Boliche.hs
+++ Boliche.hs
@@ -51,18 +51,62 @@ rescatarse horas cliente
   | horas > 3 = cliente { resistencia = resistencia cliente + 200 }
   | otherwise = cliente { resistencia = resistencia cliente + 100 }
 
--- Itinerario de Ana
-itinerarioAna :: TipoCliente -> TipoCliente
-itinerarioAna cliente =
-  let paso1 = efectoBebida JarraLoca cliente
-      paso2 = efectoBebida (Klusener "Chocolate") paso1
-      paso3 = rescatarse 2 paso2
-      paso4 = efectoBebida (Klusener "Huevo") paso3
-  in paso4
-
--- Ejecución de la consulta del itinerario de Ana
 main :: IO ()
 main = do
-  let resultado = itinerarioAna ana
-  putStrLn $ "Estado final de Ana: " ++ show resultado
-  putStrLn $ "¿Cómo está Ana? " ++ comoEsta resultado
+  putStrLn "La Noche De  Rodri Cristian y Marcos:"
+  putStrLn ""
+  putStrLn $ "¿Cómo está Rodri antes de arrancar la noche ? " ++ comoEsta rodri
+  putStrLn ""
+  putStrLn $ "¿Cómo está Cristian antes de arrancar la noche ? " ++ comoEsta cristian
+  putStrLn ""
+  putStrLn $ "¿Cómo está Marcos antes de arrancar la noche ? " ++ comoEsta marcos
+  putStrLn ""
+  putStrLn "Marcos Se  Siente Solo y quiere hacer amigos:"
+  putStrLn ""
+  -- Agregar a Ana y Rodri como amigos de Marcos
+  let marcosConAmigos = agregarAmigo (agregarAmigo marcos ana) rodri
+
+  putStrLn "Agregar a Ana y Rodri como amigos de Marcos:"
+  putStrLn $ "Estado de Marcos después de agregar a Ana y Rodri: " ++ show marcosConAmigos
+  putStrLn ""
+
+  -- Verificar el estado de Marcos después de agregar amigos
+  putStrLn $ "¿Cómo está Marcos Después de Hacer Amigos? " ++ comoEsta marcosConAmigos
+  putStrLn ""
+  putStrLn "La Noche De Ana:"
+  putStrLn ""
+  putStrLn $ "¿Cómo está Ana antes de arrancar la noche ? " ++ comoEsta ana
+  putStrLn $ "Resistencia de Ana: " ++ show (resistencia ana)
+  putStrLn $ "Resistencia de sus amigos: " ++ show (map resistencia (listaAmigos ana))
+  putStrLn ""
+
+  let resultado1 = efectoBebida JarraLoca ana
+  putStrLn $ "Estado después de tomar una jarra loca:\n" ++ show resultado1
+  putStrLn $ "Resistencia de Ana después de tomar una jarra loca: " ++ show (resistencia resultado1)
+  putStrLn $ "Resistencia de sus amigos después de tomar una jarra loca: " ++ show (map resistencia (listaAmigos resultado1))
+  putStrLn ""
+
+  let resultado2 = efectoBebida (Klusener "Chocolate") resultado1
+  putStrLn $ "Estado después de tomar un Klusener de chocolate:\n" ++ show resultado2
+  putStrLn $ "Resistencia de Ana después de tomar un Klusener de chocolate:\n" ++ show (resistencia resultado2)
+  putStrLn ""
+  putStrLn $ "El De Seguridad Se Puso La Gorra ,Te Rescatas o te tengo que sacar: "
+  putStrLn ""
+  let resultado3 = rescatarse 2 resultado2
+  putStrLn $ "Estado después de rescatarse:\n" ++ show resultado3
+  putStrLn $ "Resistencia de Ana después de rescatarse:\n" ++ show (resistencia resultado3)
+  putStrLn ""
+
+  let resultado4 = efectoBebida (Klusener "Huevo") resultado3
+  
+  putStrLn $ "Resistencia de Ana después de tomar un Klusener de huevo:\n" ++ show (resistencia resultado4)
+  putStrLn ""
+  putStrLn $ "Fue Una Noche Agitda ¿Como Terimno Ana Despues Del Boliche?"
+  putStrLn ""
+  putStrLn $ "¿Cómo está Ana? " ++ comoEsta resultado4
+  putStrLn ""
+  putStrLn $ "Estado final de Ana: \n" ++ show resultado4
+  putStrLn ""
+  putStrLn "¿Como Terminaron Los Amigos De Ana La Noche De Boliche?:"
+  mapM_ (\amigo -> putStrLn $ nombreCliente amigo ++ ": " ++ comoEsta amigo) (listaAmigos resultado4)
+
